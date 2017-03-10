c47b1e884a4f138b1c3d5f0448e6bc98b0a95ab0
SupakornChot <supakorn.flook@mail.kmutt.ac.th>
2017-03-10 14:44:34 +0700

    Add files via upload

--- Bill.java
+++ Bill.java
@@ -1,6 +1,40 @@
 
 public abstract class Bill {
     protected long billId;
-    private Costomer cust;
-    private tot
+    private Customer cust;
+    private double totalPrice;
+    private double promoPrice;
+
+    public Bill(Customer cust, double price) {
+        this.cust = cust;
+        this.totalPrice = price;
+    }
+    
+
+    public Customer getCust() {
+        return cust;
+    }
+
+    public void setCust(Customer cust) {
+        this.cust = cust;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public void setPromoPrice(Bill bill, double minPrice, double percentPro,double maxPro) {
+        this.promoPrice = promoPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Bill{" + "billId=" + billId + ", cust=" + cust + ", totalPrice=" + totalPrice + ", promoPrice=" + promoPrice + '}';
+    }
+    
+    
 }
